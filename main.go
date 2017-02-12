25d4083b8eb8e678f0dea4363098ad39cc0321fd
Dan LaMotte <lamotte85@gmail.com>
2017-02-12 15:59:26 -0600

    barely the bones on this skeleton

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+    "github.com/gzl-io/gzl/pkg/agent"
+)
+
+func main() {
+    agent.Run()
+}
---
+++ pkg/agent/main.go
@@ -0,0 +1,7 @@
+package agent
+
+import "fmt"
+
+func Run() {
+    fmt.Println("run")
+}
